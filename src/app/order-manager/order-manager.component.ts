022573141865b72a6d96e776e733d76f364987a2
ChrisHardin <chris.hardin@oasisdigital.com>
2019-01-24 13:19:10 -0600

    curriculum update

--- src/app/order-manager/order-manager.component.ts
+++ src/app/order-manager/order-manager.component.ts
@@ -1,18 +1,14 @@
 import { Component } from '@angular/core';
 
-import { fakeApiOrderData, Order } from '../api-types';
+import { Order, fakeApiOrderData } from '../api-types';
 
 @Component({
   selector: 'order-manager',
   templateUrl: './order-manager.component.html'
 })
 export class OrderManagerComponent {
-  orderList: Order[];
+  orderList: Order[] = fakeApiOrderData;
   selectedOrder: Order;
-
-  constructor() {
-    this.orderList = fakeApiOrderData;
-  }
 
   choose(i: number) {
     this.selectedOrder = this.orderList[i];
